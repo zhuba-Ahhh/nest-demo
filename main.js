689c2dea83345ae5ab2e238ec7854728efc62cad
zhuba-Ahhh <84793349+zhuba-Ahhh@users.noreply.github.com>
2024-05-02 08:49:07 +0000

    Deploying to gh-pages from @ zhuba-Ahhh/nest-demo@5e515c55b416d1b2a5d6cc5642310f8af5825057 🚀

--- main.js
+++ main.js
@@ -8,21 +8,16 @@ const _swagger = require("@nestjs/swagger");
 const _common = require("@nestjs/common");
 const _httpexceptionfilter = require("./core/filter/http-exception/http-exception.filter");
 const _transforminterceptor = require("./core/interceptor/transform/transform.interceptor");
-const startTime = new Date().getTime();
-console.info(`NestFactory ${new Date().getTime() - startTime}`);
-console.info(`AppModule耗时：${new Date().getTime() - startTime}`);
 async function bootstrap() {
     const app = await _core.NestFactory.create(_appmodule.AppModule);
     app.setGlobalPrefix('api/');
     app.useGlobalPipes(new _common.ValidationPipe()); // 参数校验 数据验证
     app.useGlobalInterceptors(new _transforminterceptor.TransformInterceptor()); // 拦截成功的返回数据
     app.useGlobalFilters(new _httpexceptionfilter.HttpExceptionFilter()); // 拦截错误请求
-    console.info(`执行至 create 耗时 ${new Date().getTime() - startTime}`);
     const config = new _swagger.DocumentBuilder().setTitle('管理后台').setDescription('管理后台接口文档').setVersion('1.0').addBearerAuth().build();
     const document = _swagger.SwaggerModule.createDocument(app, config);
     _swagger.SwaggerModule.setup('docs', app, document);
     await app.listen(3000);
-    console.info(`执行至 listen 耗时 ${new Date().getTime() - startTime}`);
 }
 bootstrap();
 

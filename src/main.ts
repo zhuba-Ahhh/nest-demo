8af44503715f3f26a42dbffc60f14a3baef409b2
zhuba-Ahhh <3477826311@qq.com>
2024-05-02 16:43:20 +0800

    test: :white_check_mark: swc

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
+const startTime = new Date().getTime();
 import { NestFactory } from '@nestjs/core';
+console.info(`NestFactory ${new Date().getTime() - startTime}`);
 import { AppModule } from './app.module';
+console.info(`AppModule耗时：${new Date().getTime() - startTime}`);
 
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
@@ -14,6 +17,7 @@ async function bootstrap() {
   app.useGlobalInterceptors(new TransformInterceptor()); // 拦截成功的返回数据
   app.useGlobalFilters(new HttpExceptionFilter()); // 拦截错误请求
 
+  console.info(`执行至 create 耗时 ${new Date().getTime() - startTime}`);
   const config = new DocumentBuilder()
     .setTitle('管理后台')
     .setDescription('管理后台接口文档')
@@ -24,5 +28,6 @@ async function bootstrap() {
   SwaggerModule.setup('docs', app, document);
 
   await app.listen(3000);
+  console.info(`执行至 listen 耗时 ${new Date().getTime() - startTime}`);
 }
 bootstrap();

36fe93330d09ca2ecc58a46c977901efdcecf34d
zhuba-Ahhh <3477826311@qq.com>
2024-09-01 13:07:48 +0800

    chore: :hammer: port配置

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.SERVER_PORT || 3000);
 }
 bootstrap();
